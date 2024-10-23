616a28e897d81c48ea581bdd27acb0419237ffcc
marita <maroosama30@gamil.com>
2024-10-23 18:32:00 +0300

    home page layout (1st draft)

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -4,7 +4,58 @@ class HomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return Scaffold(
-
+      appBar: AppBar(
+        title: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'Hedieaty',
+              style: TextStyle(
+                fontSize: 35,
+                fontWeight: FontWeight.bold,
+                color: Colors.deepPurple,
+              ),
+            ),
+            SizedBox(width: 10),
+            Icon(Icons.card_giftcard,
+                color: Colors.deepPurple,
+                size: 35,),
+          ],
+        ),
+        backgroundColor: Colors.lightBlueAccent,
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search,
+            size: 30,),
+            onPressed: () {
+              // search logic goes here
+            },
+          )
+        ],
+        // Ensuring title remains centered
+      ),
+      body: Column(
+        children: [
+          ListTile(
+            title: Text("Friend's Name"),
+            subtitle: Text("Upcoming Events: 1"),
+            leading: CircleAvatar(
+              backgroundImage: AssetImage('asset/Female_Icon (2).png'),
+            ),
+            onTap: () {
+              Navigator.pushNamed(context, '/events');
+            },
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/events');
+              },
+              child: Text('Create an Event'),
+          )
+        ],
+      ),
     );
   }
 }
