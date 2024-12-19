85478650855742a2d372b0883d2e669705d9ccfb
marita <maroosama30@gamil.com>
2024-12-19 17:00:42 +0200

    app title animation

--- lib/screens/welcome_page.dart
+++ lib/screens/welcome_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 import 'package:hedieaty/screens/login2.dart';
 import 'package:hedieaty/screens/login_page.dart';
-import 'dart:async';
 import 'home_page.dart';
 
 class WelcomePage extends StatefulWidget {
@@ -9,13 +9,31 @@ class WelcomePage extends StatefulWidget {
   _WelcomePageState createState() => _WelcomePageState();
 }
 
-class _WelcomePageState extends State<WelcomePage> {
+class _WelcomePageState extends State<WelcomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
   @override
   void initState() {
     super.initState();
 
-    // Navigate to home page after 3 seconds
-    Timer(Duration(seconds: 3), () {
+    // Initialize the animation controller
+    _controller = AnimationController(
+      duration: const Duration(seconds: 3),
+      vsync: this,
+    );
+
+    // Define the bounce animation
+    _animation = Tween<double>(begin: 0, end: -30).chain(
+      CurveTween(curve: Curves.elasticOut),
+    ).animate(_controller);
+
+    // Start the animation
+    _controller.forward();
+
+    // Navigate to login screen after 3 seconds
+    Timer(const Duration(seconds: 3), () {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => LoginScreen()),
@@ -24,29 +42,43 @@ class _WelcomePageState extends State<WelcomePage> {
   }
 
   @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white, // Customize background color
       body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Hedieaty',
-              style: TextStyle(
-                fontSize: 45,
-                fontWeight: FontWeight.bold,
-                color: Colors.deepPurple,
-                fontFamily: "Parkinsans",
+        child: AnimatedBuilder(
+          animation: _animation,
+          builder: (context, child) {
+            return Transform.translate(
+              offset: Offset(0, _animation.value),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Hedieaty',
+                    style: TextStyle(
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.deepPurple,
+                      fontFamily: "Parkinsans",
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  Icon(
+                    Icons.card_giftcard,
+                    size: 65,
+                    color: Colors.deepPurple,
+                  ),
+                ],
               ),
-            ),
-            SizedBox(height: 20),
-            Icon(
-              Icons.card_giftcard,
-              size: 65,
-              color: Colors.deepPurple,
-            ),
-          ],
+            );
+          },
         ),
       ),
     );
